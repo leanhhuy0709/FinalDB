b7ecba0b5fb02d66a8bcadfe69ff4f3bd1e3f73c
leanhhuy0709 <huy.leanh0709@hcmut.edu.vn>
2022-12-11 11:02:29 +0700

    update App

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import ErrorPage from './page/ErrorPage/ErrorPage';
 function App() {
     //thông tin mà người dùng đã đăng nhập
     const [user, setUser] = useState({"id": 1, "username":"huy123", "FName": "Le", "MName": "Anh", "LName": "Huy", "BDate": "2002-09-07", "Email": "huy123@gmail.com", "Gender": "Male", "Age": 20});
-    React.useEffect(() => {
+    /*React.useEffect(() => {
       axios.get('/info', {}, {headers: {token: localStorage.getItem("token")}})
       .then(function (response) {
         //console.log(response.data);
@@ -24,7 +24,7 @@ function App() {
       .catch(function (error) {
         console.log(error);
       });
-      }, []);
+      }, []);*/
 
     const [foods, setFoods] = useState([
       {"Res_id": 1, "F_id": 1, "Type_ID": 3, "Fname": "com suon bi", "Unit_price": 30000, "Description": "1 suon+bi", "Status": 1, "Size": "M", "PictureURL": "https://kenh14cdn.com/2018/6/13/2954314317567482510571071022300657098424320n-15288595313631828141175-15288609206141257248658.png"},
@@ -35,7 +35,7 @@ function App() {
       {"Res_id": 4, "F_id": 6, "Type_ID": 3, "Fname": "tra sua truyen thong", "Unit_price": 25000, "Description": "1 suon+bi", "Status": 1, "Size": "M", "PictureURL": "https://kenh14cdn.com/2018/6/13/2954314317567482510571071022300657098424320n-15288595313631828141175-15288609206141257248658.png"},
       {"Res_id": 5, "F_id": 7, "Type_ID": 2, "Fname": "tra sua olong", "Unit_price": 30000, "Description": "1 suon+bi", "Status": 1, "Size": "M", "PictureURL": "https://kenh14cdn.com/2018/6/13/2954314317567482510571071022300657098424320n-15288595313631828141175-15288609206141257248658.png"},
     ]);
-    React.useEffect(() => {
+    /*React.useEffect(() => {
       axios.get('/foods')
       .then(function (response) {
         setFoods(response.data);
@@ -43,7 +43,7 @@ function App() {
       .catch(function (error) {
         console.log(error);
       });
-      }, []);
+      }, []);*/
     const [combos, setCombos] = useState([
       {"Cb_id": 1, "Cb_name": "Combo 1", "Cost": 30000, "Description": "1 suon+bi", "Status": 1, "PictureURL": "https://cdn.cet.edu.vn/wp-content/uploads/2018/01/combo-la-gi.jpg"},
       {"Cb_id": 2, "Cb_name": "com suon bi trung", "Cost": 35000, "Description": "1 suon+bi+trung", "Status": 1, "PictureURL": "https://cdn.cet.edu.vn/wp-content/uploads/2018/01/combo-la-gi.jpg"},
@@ -53,7 +53,7 @@ function App() {
       {"Cb_id": 6, "Cb_name": "tra sua truyen thong", "Cost": 25000, "Description": "1 suon+bi", "Status": 1, "PictureURL": "https://cdn.cet.edu.vn/wp-content/uploads/2018/01/combo-la-gi.jpg"},
       {"Cb_id": 7, "Cb_name": "tra sua olong", "Cost": 30000, "Description": "1 suon+bi", "Status": 1, "PictureURL": "https://cdn.cet.edu.vn/wp-content/uploads/2018/01/combo-la-gi.jpg"},
     ]);
-    React.useEffect(() => {
+    /*React.useEffect(() => {
       axios.get('/combos')
       .then(function (response) {
         setCombos(response.data);
@@ -61,7 +61,7 @@ function App() {
       .catch(function (error) {
         console.log(error);
       });
-      }, []);
+      }, []);*/
     const [cart, setCart] = useState([
       {"Res_id": 1, "F_id": 1, "Type_ID": 3, "Fname": "com suon bi", "Unit_price": 30000, "Description": "1 suon+bi", "Status": 1, "Size": "M", "PictureURL": "https://kenh14cdn.com/2018/6/13/2954314317567482510571071022300657098424320n-15288595313631828141175-15288609206141257248658.png", "quantity": 4},
       {"Res_id": 1, "F_id": 2, "Type_ID": 4, "Fname": "com suon bi trung", "Unit_price": 35000, "Description": "1 suon+bi+trung", "Status": 1, "Size": "L", "PictureURL": "https://kenh14cdn.com/2018/6/13/2954314317567482510571071022300657098424320n-15288595313631828141175-15288609206141257248658.png", "quantity": 5},
@@ -71,7 +71,7 @@ function App() {
       {"Res_id": 4, "F_id": 6, "Type_ID": 3, "Fname": "tra sua truyen thong", "Unit_price": 25000, "Description": "1 suon+bi", "Status": 1, "Size": "M", "PictureURL": "https://kenh14cdn.com/2018/6/13/2954314317567482510571071022300657098424320n-15288595313631828141175-15288609206141257248658.png", "quantity": 2},
       {"Res_id": 5, "F_id": 7, "Type_ID": 2, "Fname": "tra sua olong", "Unit_price": 30000, "Description": "1 suon+bi", "Status": 1, "Size": "M", "PictureURL": "https://kenh14cdn.com/2018/6/13/2954314317567482510571071022300657098424320n-15288595313631828141175-15288609206141257248658.png", "quantity": 3},
     ]);
-    React.useEffect(() => {
+    /*React.useEffect(() => {
       axios.get('/cart')
       .then(function (response) {
         setFoods(response.data);
@@ -79,7 +79,7 @@ function App() {
       .catch(function (error) {
         console.log(error);
       });
-      }, []);
+      }, []);*/
 
     const [users, setUsers] = useState([
       {"Cus_id": 1, "username":"huy123", "FName": "Le", "MName": "Anh", "LName": "Huy", "BDate": "2002-09-07", "Email": "huy123@gmail.com", "Gender": "Male", "Age": 20, "Rank": 1, "Mem_id": 12, "Accumulated_point": 100, "Registration_date": "2023-01-01"},
@@ -88,7 +88,7 @@ function App() {
       {"Cus_id": 4, "username":"chuy123", "FName": "Le", "MName": "Anh", "LName": "Chuy", "BDate": "2002-06-07", "Email": "chuy123@gmail.com", "Gender": "Female", "Age": 20, "Rank": 6, "Mem_id": 15, "Accumulated_point": 100, "Registration_date": "2023-01-01"},
       {"Cus_id": 5, "username":"ahuy123", "FName": "Nguyen", "MName": "Anh", "LName": "Huy", "BDate": "2002-05-07", "Email": "ahuy123@gmail.com", "Gender": "Male", "Age": 20, "Rank": 8, "Mem_id": 16, "Accumulated_point": 100, "Registration_date": "2023-01-01"},
     ]);
-    React.useEffect(() => {
+    /*React.useEffect(() => {
       axios.get('/GetAllCus', {
           token: localStorage.getItem("token")
         })
@@ -99,7 +99,7 @@ function App() {
       .catch(function (error) {
         console.log(error);
       });
-      }, []);
+      }, []);*/
 
 
     function Search() {
